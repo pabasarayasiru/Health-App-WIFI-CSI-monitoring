617414feb81333d3df00225129d96ee08c08f8b8
pabasarayasiru <pabasara.yasiru@gmail.com>
2026-04-02 16:56:44 +0530

    fixed issues

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,7 +1,12 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
 
-var serviceAccount = require("../../serviceAccountKey.json");
+// var serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
