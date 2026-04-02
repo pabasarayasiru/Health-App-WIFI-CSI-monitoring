a1fcaa8fc9ec0bd8dc2051deced57a23115a5260
pabasarayasiru <pabasara.yasiru@gmail.com>
2026-04-02 15:56:05 +0530

    fixed issues in connections

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,8 +11,6 @@ const server = http.createServer(app);
 setupSocket(server);
 
 
-
-
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
